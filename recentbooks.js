421fe16927462cc2123f71785b7678a8ccca08cb
maryboamah <maryboamah680@gmail.com>
2021-12-28 22:36:54 -0500

    adding data

--- recentbooks.js
+++ recentbooks.js
@@ -7,45 +7,56 @@
 const url = `https://www.googleapis.com/books/v1/volumes?q=new&key=AIzaSyBwnJ8SVW--D44GOqK3qQU3ZP6TEOAYQuY`
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=>{
-        let dogpic = data
+    .then(async(data)=>{
+        let bookdata = data
      
-        console.log(dogpic)
-        let item = dogpic.items
-        console.log(item[0].volumeInfo.title)
-        console.log(item[0].volumeInfo.authors.toString())
-        console.log(item[0].volumeInfo.imageLinks.smallThumbnail)
-        console.log(item[0].volumeInfo.previewLink)//embededone
-        let go= item[0].volumeInfo.authors.toString()
-        // imageDiv.innerHTML = item[0].volumeInfo.authors.toString()
-
-        // document.getElementById("test").innerHTML =`<img src =${item[0].volumeInfo.imageLinks.smallThumbnail} alt="book image"> <h1> ${item[0].volumeInfo.title} </h1> <h2>${item[0].volumeInfo.authors.toString()}</h2>`
-        let startDiv = `<div class="recent">` 
-        item.map(function(item){
+        
+        let item = bookdata.items
+        
+        let startDiv = item.map(function(item){
            
             // let q = document.getElementsByClassName("recentimg")
-            console.log(item.volumeInfo.imageLinks.smallThumbnail)
-            let thumb = item.volumeInfo.imageLinks.thumbnail
-            console.log(thumb)
-            return startDiv += `<div class="recentimg"><img src="${thumb}" alt="bookimage"></div>` + `<div class="recentinfo">`+ `<div class="recentinfoTitle">${item.volumeInfo.title}</div>
-            <div class="recentinfoAuthor">${item.volumeInfo.authors.toString()} here</div>
-            <div class="recentinfoPublisher">${item.volumeInfo.publisher}</div>
-            <div class="recentinfoPubDate">${item.volumeInfo.publishedDate}</div>
-            <div class="recentinfoReviews">${item.volumeInfo.pageCount}</div>
-            <div class="recentinfolang">${item.volumeInfo.language}</div>
-            <div class="recentinfoPreview">${item.volumeInfo.previewLink}</div>` + `</div>`
+            
+            let volumeInfo = item.volumeInfo
+           
+            return  `<div class = "allrecent"><div class="recentimg">`+
+            `<img src="${volumeInfo.imageLinks.thumbnail}" alt="bookimage"></div>` + `<div class="recentinfo">`+ `<div class="recentinfoTitle">
+            ${item.volumeInfo.title}</div><div class="recentinfoAuthor">${volumeInfo.authors.toString()} here</div>
+            <div class="recentinfoPublisher">${volumeInfo.publisher}</div>
+            <div class="recentinfoPubDate">${volumeInfo.publishedDate}</div>
+            <div class="recentinfoReviews">${volumeInfo.pageCount}</div>
+            <div class="recentinfolang">${volumeInfo.language}</div>
+            <div class="recentinfoPreview">${volumeInfo.previewLink}</div>` + `</div></div>`
+            
             
         });
+        let timeInterval = 700
+     
 
-        startDiv +=`</div>`
-        document.getElementById("recent1").innerHTML=startDiv
+    for(let i = 0; i<startDiv.length; i++){
+        timeInterval+= 300
+         startDiv[i]
+      
+        let display = document.getElementById("recent1").innerHTML = startDiv[i]
+
+         new Promise.all(resolve => setInterval(() => resolve(display), timeInterval));
+        
+        // setInterval(display, timeInterval)
+}
+
+
+   
+        //  document.getElementById("recent1").innerHTML= display()
 
 
 
       
 
     })
+   
     .catch((error)=>{
         console.log(error)
     })
+
+    
 
