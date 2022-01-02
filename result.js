be2ffb4e9ec750a16b8c08ef626747ee0ef1682b
maryboamah <maryboamah680@gmail.com>
2022-01-02 14:23:40 -0500

    adding toggle event

--- result.js
+++ result.js
@@ -7,6 +7,7 @@ const userSearchSelectionup = localStorage.getItem('SELECTION')
 const userSearchValueup = localStorage.getItem('SEARCH')
 if (userSearchValueup!="" && userSearchSelectionup !=""){
     const url = `https://www.googleapis.com/books/v1/volumes?q=${userSearchValueup}+${userSearchSelectionup}&maxResults=20&key=AIzaSyBwnJ8SVW--D44GOqK3qQU3ZP6TEOAYQuY`
+    document.getElementById("recentHead").innerHTML = `Your Search result for "${userSearchValueup}" in " ${userSearchSelectionup}" . `
     console.log(url)
         fetch(url)
         .then((res)=>res.json())
@@ -16,30 +17,29 @@ if (userSearchValueup!="" && userSearchSelectionup !=""){
             
             let item = bookdata.items
             
-            let startDiv = item.map(function(item){
+            let searchResultsAll = item.map(function(item){
                
                 // let q = document.getElementsByClassName("recentimg")
                 
                 let volumeInfo = item.volumeInfo
                console.log()
-                return  ` <div class="row1">
-                <div class="row1image"><img src="${volumeInfo.imageLinks.thumbnail}" alt="book cover"></div>
-                <div class="title">${volumeInfo.title}</div>
-             </div>
-             <div class="row2">
-                 <div class="author">Author(s): ${volumeInfo.authors}</div>
-                 <div class="publishdate">Publication Date: ${volumeInfo.publishedDate}</div>
-                 <div class="averagerating">Rating: ${volumeInfo.averageRating}</div>
-                 <div class="publisher">Publisher: ${volumeInfo.publisher}</div>
-                 <div class="pagecount">Number of Pages: ${volumeInfo.pageCount}</div>
-                 <div class="previewLink">Preview on Google: <a href="${volumeInfo.previewLink}">${volumeInfo.previewLink}</a> </div>
-                 <div  class="description">Description: ${volumeInfo.description}</div>
-             </div>`
-                
+               return  ` <div class="row1">
+               <div class="row1image"><img src="${volumeInfo.imageLinks.thumbnail}" alt="book cover" style="width: 200px; height:200px;"></div>
+               <div class="title">${volumeInfo.title}</div>
+            </div>
+            <div class="row2">
+                <div class="head"><p class="allheadings">Author(s):</p> <span>${volumeInfo.authors}<span></div>
+                <div class="head"><p class="allheadings">Publication Date:</p> ${volumeInfo.publishedDate}</div>
+                <div class="head"> <p class="allheadings">Rating:</p> ${volumeInfo.averageRating}</div>
+                <div class="head"><p class="allheadings">Publisher:</p> ${volumeInfo.publisher}</div>
+                <div class="head"><p class="allheadings">Number of Pages:</p> ${volumeInfo.pageCount}</div>
+                <div class="head"><p class="allheadings">Preview on Google:</p> <a href="${volumeInfo.previewLink}">${volumeInfo.previewLink}</a> </div>
+                <div  class="head"><p class="allheadings">Description:</p> ${volumeInfo.description}</div>
+            </div>`
                 
             });
            
-            document.getElementById("bookresult").innerHTML = startDiv
+            document.getElementById("bookresult").innerHTML = searchResultsAll
          
     
     
@@ -76,6 +76,7 @@ if (userSearchValue!="" && userSearchSelection !=""){
         .then((data)=>{
             let bookdata = data
          
+           
             
             let item = bookdata.items
             
@@ -86,32 +87,50 @@ if (userSearchValue!="" && userSearchSelection !=""){
                 let volumeInfo = item.volumeInfo
                console.log()
                 return  ` <div class="row1">
-                <div class="row1image"><img src="${volumeInfo.imageLinks.thumbnail}" alt="book cover"></div>
+                <div class="row1image"><img src="${volumeInfo.imageLinks.thumbnail}" alt="book cover" style="width: 200px; height:200px;"></div>
                 <div class="title">${volumeInfo.title}</div>
              </div>
              <div class="row2">
-                 <div class="author">Author(s): ${volumeInfo.authors}</div>
-                 <div class="publishdate">Publication Date: ${volumeInfo.publishedDate}</div>
-                 <div class="averagerating">Rating: ${volumeInfo.averageRating}</div>
-                 <div class="publisher">Publisher: ${volumeInfo.publisher}</div>
-                 <div class="pagecount">Number of Pages: ${volumeInfo.pageCount}</div>
-                 <div class="previewLink">Preview on Google: <a href="${volumeInfo.previewLink}">${volumeInfo.previewLink}</a> </div>
-                 <div  class="description">Description: ${volumeInfo.description}</div>
+                 <div class="head"><p class="allheadings">Author(s):</p> <span>${volumeInfo.authors}<span></div>
+                 <div class="head"><p class="allheadings">Publication Date:</p> ${volumeInfo.publishedDate}</div>
+                 <div class="head"> <p class="allheadings">Rating:</p> ${volumeInfo.averageRating}</div>
+                 <div class="head"><p class="allheadings">Publisher:</p> ${volumeInfo.publisher}</div>
+                 <div class="head"><p class="allheadings">Number of Pages:</p> ${volumeInfo.pageCount}</div>
+                 <div class="head"><p class="allheadings">Preview on Google:</p> <a href="${volumeInfo.previewLink}">${volumeInfo.previewLink}</a> </div>
+                 <div  class="head"><p class="allheadings">Description:</p> ${volumeInfo.description}</div>
              </div>`
-                
+             
                 
             });
+            function displayDetails(){
+
+                var row1 = document.querySelectorAll(".row1")
+                var row2 = document.querySelectorAll(".row2")
+
+                for(let i =0; i<row1.length; i++){
+                    row1[i].addEventListener('mouseover', function(){
+
+                        row2[i].classList.toggle("hidedetails")
+                        
+                        
+                    })
+
+                }
+              
+                
+               
+            }
+           
            
             document.getElementById("bookresult").innerHTML = startDiv
-         
-    
-    
-    
+           
+                    
+            
        
              //  document.getElementById("recent1").innerHTML= display()
     
     
-    
+             displayDetails()
           
     
         })
@@ -126,6 +145,8 @@ if (userSearchValue!="" && userSearchSelection !=""){
     
 }
 
+ 
 
 
-    
+
+   
